a10b236399bf4f54a27e0b3908192feaa9471eb9
apple <apple@apples-MacBook-Air.local>
2025-03-10 07:02:02 -0700

    Initial Commit

---
+++ src/main/java/org/example/TestClass.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass{
+    public static void main(String [] args){
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        WebElement search = driver.findElement(By.xpath("//textarea[@class='gLFyf']"));
+        search.sendKeys("Flipkart.com");
+    }
+}
